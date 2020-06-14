02c5378dbe89449a77552285cb912af81e7954d2
Samuel Adjei <samueladj3i@gmail.com>
2020-06-14 16:27:33 +0100

    improved modal animation

--- js/app.js
+++ js/app.js
@@ -1,20 +1,31 @@
-// Modal
-// Get Modal
-let modal = document.querySelector(".modal-content");
-// Get the button that opens the modal
-let btn = document.querySelector(".modal-btn");
-// Get the <a> element that closes the modal
-let close = document.querySelector(".close");
-// When the user clicks the GamepadButton, open the modal
-btn.addEventListener("click", function (e) {
-	e.preventDefault();
-	modal.style.display = "block";
+// // Modal
+// // Get Modal
+// const modal = document.querySelector(".modal-inner");
+// // Get the button that opens the modal
+// const btn = document.querySelector(".modal-btn");
+// // Get the <a> element that closes the modal
+// const close = document.querySelector(".close");
+// // When the user clicks the GamepadButton, open the modal
+// btn.addEventListener("click", function (e) {
+// 	e.preventDefault();
+// 	modal.style.display = "block";
+// });
+
+// // Get the Close button
+// close.addEventListener("click", function (e) {
+//   e.preventDefault();
+// 	modal.style.display = "none";
+// });
+
+// MODAL
+const modalBtn = document.querySelector(".modal-btn");
+const modalOuter = document.querySelector(".modal-outer");
+const close = document.querySelector(".close");
+
+modalBtn.addEventListener("click", (e) => {
+	modalOuter.classList.add("open");
 });
 
-
-// Get the Close button 
-close.addEventListener("click", function (e) {
-  e.preventDefault();
-	modal.style.display = "none";
+close.addEventListener("click", (e) => {
+	modalOuter.classList.remove("open");
 });
-
